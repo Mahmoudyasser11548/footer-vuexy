76cf48484db29af17fd8631b077f2d8eede5ca6d
Mahmoud.yasser <Mahmoud.yasser@tds-me.com>
2023-08-06 18:22:19 +0200

    Modify to jsx

--- src/footer-footer-vuexy.jsx
+++ src/footer-footer-vuexy.jsx
@@ -1,12 +1,19 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 import ReactDOM from "react-dom";
 import singleSpaReact from "single-spa-react";
 import App from "./App.jsx";
+import { Provider } from 'react-redux'
+import { store } from "./redux/store.js"
 
 const lifecycles = singleSpaReact({
   React,
   ReactDOM,
-  rootComponent: App,
+  rootComponent: () => (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  ),
   errorBoundary(err, info, props) {
     // Customize the root error boundary for your microfrontend here.
     return null;
